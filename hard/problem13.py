f37801b1c516ddd4033db51d69b8c9fc969eddd4
shenez <shenez54321@gmail.com>
2026-09-09 12:47:47 +0530

    .

---
+++ hard/problem13.py
@@ -0,0 +1,44 @@
+"""
+13. Longest Valid Parentheses
+
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: s = "(()"
+
+Output: 2
+
+Explanation: The longest valid parentheses substring is "()".
+
+
+Example 2:
+
+Input: s = ")()())"
+
+Output: 4
+
+Explanation: The longest valid parentheses substring is "()()".
+
+
+Example 3:
+
+Input: s = ""
+
+Output: 0
+"""
+
+def pred(s):
+    stack = []
+    valid_stack = []
+    for i in range(len(s)):
+        # if s[i] == ")" and i == 0:
+        #     continue
+        # if s[i] ==")" and stack[]
+        
+    
+    
+    
+        
+
+print(pred(")()())"))
