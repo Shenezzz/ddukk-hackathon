e96e57a4b7542dcfb931c661c4306fb5df8bd3cb
Fsparx <finanm452@gmail.com>
2026-09-09 12:36:38 +0530

    problem2

--- medium/problem2.py
+++ medium/problem2.py
@@ -33,5 +33,15 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 """
 
 def findstring(s):
-    return s
-print(findstring("abcabcbb"))
+    sub=s[0]
+    left=0
+    maxx=0
+    for right in range(1,len(s)):
+        maxx=max(maxx,len(sub))
+        while(s[right] in sub):
+            left+=1
+            sub=sub[left:right+1]
+        sub+=s[right]
+    return maxx
+
+print(findstring("bbbbb"))
